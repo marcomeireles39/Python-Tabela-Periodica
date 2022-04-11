e0612ba29198fa5a7adb394126d8f093ef1214d4
Marco Aurélio Santos Meireles <masmfederal@gmail.com>
2022-04-11 11:32:56 -0300

    Postada com exito

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # Python Tabela Periodica
- classe que carrega informações sobre elementos da tabela periodica
+classe responsável por acessar informações da API periodic-table-api-go
+e facilitar a obtenção de dados da tabela periodica e dar a opção de
+criar um banco de dados sqlite para guardar todas as informações:
+
+__arquivo TPeriodica.py__
+
+__Contem toda a estrutura da nossa classe__
+
+classes utilizadas
+
+    import json - Biblioteca pra adequação do código json recebido
+
+    import requests - Biblioteca responsável pela conexão e obtenção dos dados de retorno
+    
+    from Bd import Bd_sql - Essa biblioteca foi a que criei responsável por criar um banco de dados
+                            com as informações que eu quero gravar no caso um banco de dados com os
+                            dados obtidos sobre os elementos da tabela periodica.
+
+
+
+__Metodos da classe__
+
+    obj_ini.carregar() - Método responsável por carregar os dados do elemento que será feita a pesquisa.
+                         A pesquisa será em com o numero atômico do elemento que será inserido quando
+                         ao criar o objeto:
+                            obj_ini.Tabela_Periodica(nAtomico)    
+    
+    obj_ini.info() - Método que retorna todos os dados do elemento químicos em um dicionário qualquer dúvida
+                     estude os dados de retorno nessa classe.
+    
+
+    obj_ini.to_sqlite(dic, cindice, cvalor) - Esse método possibilita que os dados obtidos das pesquisas
+                                                    sejam salvas em um banco de dados sqlite e funciona da
+                                                    seguinte forma:
+                        - dic - Discionario retornado da pesquisa
+                        - cindice True ou False - True : Habilita a inserção do indice na tabela
+                                                False : Desabilita a inserção do indice na tabela
+
+
+                        - cvalor True ou False - True : Habilita a inserção dos valores na tabela
+                                                False : Desabilita a inserção dos valores na tabela
+
+__Como usar :__
+
+        from TPeriodica import Tabela_Periodica - importa a classe Tabela_Periodica
+
+        tp = Tabela_Periodica(2) - Instancia a classe e já insere o número atômico do elemento
+        
+        tp.carregar() - Carrega os dados da API externa
+
+        elemento =tp.info() - Retorno das informações em um dicionário
+
+        tp.to_sqlite(elemento, True, True) - caso queira pode jogar a informação em um banco de dados
+                                              já criado ou que ainda não criou, caso ainda não criou
+                                              observado sempre os parametros parametros de indice e valores.
+
+
+__OBS:__
+Os dados da biblioteca Bd.py poderam ser visto em um dos repositórios aqui criados.
---
+++ TPeriodica.py
@@ -0,0 +1,69 @@
+# Arquivo : TPeriodica 
+# Criador : Marco Aurélio
+# Data : 10-04-2022
+
+import json
+import requests
+from Bd import Bd_sql
+class Tabela_Periodica:
+
+    """_summary_
+    classe responsavel por acessar informações da API periodic-table-api-go
+    e facilitar a obtenção de dados da tabela periodica
+    """
+    conteudo = None
+    Natomico = int(0)   
+    def __init__(self, natomico):
+        """_sumário_
+        Método construtor que precisa do parametro natomico ou numero atômico do elemento
+        
+        Args:
+            natomico (_integer_): numero atômico
+        """
+        self.Natomico = natomico
+    
+    def carregar(self):
+        
+        """__Sumário__
+        Método responsável por se conectar a API e pegar as informações
+        """
+        
+        r = requests.get('https://periodic-table-api.herokuapp.com/atomicNumber/{}'.format(self.Natomico))
+        jon_tab = json.loads(r.text)
+        self.conteudo = jon_tab
+        
+
+    def info(self):
+        diciona = {"numero_atômico":self.conteudo['atomicNumber'], "massa_atômica": self.conteudo["atomicMass"],
+                   "raio_atômico":self.conteudo["atomicRadius"], "ponto_ebulição":self.conteudo["boilingPoint"],          
+                   "tipo_ligação":self.conteudo["bondingType"], "cor":self.conteudo["cpkHexColor"],
+                   "densidade":self.conteudo["density"], "afinidade_eletrônica":self.conteudo["electronAffinity"],
+                   "eletro_negatividade":self.conteudo["electronegativity"], "config_eletrônica":self.conteudo["electronicConfiguration"],
+                   "grupo":self.conteudo["groupBlock"], "raio_ion":self.conteudo["ionRadius"],
+                   "energia_ionização":self.conteudo["ionizationEnergy"], "ponto_fusão":self.conteudo["meltingPoint"],
+                   "nome":self.conteudo["name"], "estado_oxidação":self.conteudo["oxidationStates"],
+                   "estado_padrão":self.conteudo["standardState"], "simbolo":self.conteudo["symbol"],
+                   "raio_van_del":self.conteudo["vanDelWaalsRadius"], "ano_descoberta":self.conteudo["yearDiscovered"]
+                   }
+        return diciona
+    
+    def to_sqlite(self, dic, cindice, cvalor):
+        indice = ''
+        valor = ''
+        i = int(0)
+        for di in dic:
+            i += 1
+            if (i < len(dic)):
+                if (cindice == True):
+                    indice +='{} text, '.format(di)
+                if (cvalor == True):
+                    valor += "'{}', ".format(dic[di])
+            else:
+                if (cindice == True):
+                    indice +='{} text '.format(di)
+                if (cvalor == True):                    
+                    valor += "'{}' ".format(dic[di])
+        
+        db = Bd_sql('tabela_periodica.db','elementos') 
+        db.Criar_bd(indice)
+        db.Incluir_dados(valor)
